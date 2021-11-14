acde965d7a8849f78ec88df58e7803badaa28627
halaveika <halaveika1988@gmail.com>
2021-11-14 18:57:21 +0300

    feat: add createStreamPipoe

--- src/utils/configParser.js
+++ src/utils/configParser.js
@@ -2,12 +2,15 @@ import Rot8Transform from '../customStreams/rot8Transform.js';
 import CaesarCipherTransform from '../customStreams/caesarCipherTransform.js';
 import AtbashTransform from '../customStreams/AtbashTransform.js';
 const transformStreamsStore =new Map([
-  ['A',new AtbashTransform()],
-  ['R1',new Rot8Transform('1')],
-  ['R0',new Rot8Transform('0')],
-  ['C1',new CaesarCipherTransform('1')],
-  ['C0',new CaesarCipherTransform('0')]])
+  ['A',{class:AtbashTransform}],
+  ['R1',{Rot8Transform,type:'1'}],
+  ['R0',{class:Rot8Transform,type:'0'}],
+  ['C1',{class:CaesarCipherTransform,type:'1'}],
+  ['C0',{class:CaesarCipherTransform,type:'0'}]])
 
 export default function configParser(config) {
-  return config.split('-').map(stream => transformStreamsStore.get(stream));
+  return config.split('-').map(stream => {
+    const transform =transformStreamsStore.get(stream);
+    return (transform.type) ? new transform.class(transform.type) : new transform.class()
+  });
 }
--- src/utils/creatStreamPipe.js
+++ src/utils/creatStreamPipe.js
@@ -4,30 +4,55 @@ import AtbashTransform from '../customStreams/AtbashTransform.js';
 import customReadable from '../customStreams/readable.js';
 import customWritable from '../customStreams/writable.js';
 import configParser from '../utils/configParser.js';
+import {pipeline} from 'stream';
 
 
 export default function creatStreamPipe(object){
   const cliObject = Object.assign({},object);
-  let resultPipe;
   let input$;
   let output$;
+  let transform$;
   configParser(cliObject.config);
   if (cliObject.input) {
     input$ = new customReadable(cliObject.input);
   } else {
     input$ = process.stdin;
   }
-  resultPipe = input$;
   if (cliObject.config) {
-    configParser(cliObject.config).forEach(
-      transformStream => resultPipe.pipe(transformStream)
-    );
+    transform$ = configParser(cliObject.config);
   }
   if (cliObject.output) {
     output$ = new customWritable(cliObject.output);
   } else {
     output$ = process.stdout;
   }
-  resultPipe.pipe(output$);
+  if(transform$){
+    pipeline(
+      input$,
+      ...transform$,
+      output$,
+      (err) => {
+        if (err) {
+          console.error('Pipeline failed.', err);
+        } else {
+          console.log('Pipeline succeeded.');
+        }
+      }
+      )
+  } else {
+    pipeline(
+      input$,
+      output$,
+      (err) => {
+        if (err) {
+          console.error('Pipeline failed.', err);
+        } else {
+          console.log('Pipeline succeeded.');
+        }
+      }
+      )
+  }
+
+
 }
 
