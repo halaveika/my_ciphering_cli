635f236a57e5a2ebae240b2dbac97ba58f84000e
halaveika <halaveika1988@gmail.com>
2021-11-14 23:38:34 +0300

    feat: add perfect validation

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,15 +1,60 @@
 import * as fs from 'fs';
-import transformStreamsStore from './transformStreamsStore.js';
+import BaseError from '../customError/baseError.js';
 
 class Validator{
   configCheck = (config) =>{
     if(!config) {
-      process.stderr.write(`Config is empty!\n`);
+
+    }
+    const resultArr = config.split('-');
+
+    const isFormated = resultArr.every((item => item.length < 3))
+    if (!isFormated ) {
+      process.stderr.write(`config should be this pattern {XY(-)}n\n`);
       process.exit(1);
     }
-    const result = config.split('-').every((stream => transformStreamsStore.has(stream)))
-    if (!result) {
-      throw new Error('invalid config');
+
+    const isValidX = resultArr.every((item => item[0] === 'A' || item[0] === 'R' || item[0] === 'C'))
+    if (!isValidX ) {
+      process.stderr.write(`first argument X in config pattern {XY(-)}n is not valid A or R or C\n`);
+      process.exit(1);
+    }
+
+    const isValidLength = resultArr.every(item =>{
+      if(item[0] === 'R' || item[0] === 'C') {
+        if(item.length === 2) {return true
+        } else { return false} 
+      }
+      return true;
+    }
+     )
+    if (!isValidLength) {
+      process.stderr.write(`ROT8 and Caesar cipher should have second argument Y in pattern {XY(-)}n\n`);
+      process.exit(1);
+    }
+    const isValidAtbash = resultArr.every(item =>{
+      if(item[0] === 'A') {
+        if(item.length === 1) {return true
+        } else { return false} 
+      }
+      return true;
+    }
+     )
+    if (!isValidAtbash ) {
+      process.stderr.write(`Atbash cipher should not have second argument Y in pattern {XY(-)}n\n`);
+      process.exit(1);
+    }
+    const isValidY = resultArr.every(item =>{
+      if(item.length === 2) {
+        if(item[1] === '1' || item[1] === '0' ) {return true
+        } else { return false} 
+      }
+      return true;
+    }
+     )
+    if (!isValidY ) {
+      process.stderr.write(`Second argument Y in pattern {XY(-)}n should be only 1 or 0\n`);
+      process.exit(1);
     }
   }
 
@@ -18,7 +63,7 @@ class Validator{
       if (error) {
      
         if(type === '-i' || type === '--input' ) {
-          process.stderr.write(`Intput file ${path}  do not exist!\n`);
+          process.stderr.write(`Intput file ${path}  do not exist!\n`)
           process.exit(1);
         } else {
           process.stderr.write(`Otput file ${path} do not exist!\n`);
@@ -44,6 +89,5 @@ class Validator{
   }
 
 }
-
 
 export default new Validator();
